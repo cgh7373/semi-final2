12bca8ad953799bf69b0fdbaae284ea8fbdb4f44
yhd <ehd8216@naver.com>
2024-08-20 08:55:52 +0900

    hyeondong브랜치로 날짜별 결제내역 리스트 출력 커밋

--- Mingles/src/main/java/com/kh/shop/model/dao/ShopDao.java
+++ Mingles/src/main/java/com/kh/shop/model/dao/ShopDao.java
@@ -115,6 +115,75 @@ private Properties prop = new Properties();
 		}
 		return result;
 	}
+	
+	
+	public int selectSearchListCount(Connection conn, int userNo, String startDate, String endDate) {
+		int listCount = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectSearchListCount");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, userNo);
+			pstmt.setString(2, startDate);
+			pstmt.setString(3, endDate);
+			
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				listCount = rset.getInt("count");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return listCount;
+	}
+	
+	public ArrayList<EggPayMent> selectSearchList(Connection conn, PageInfo pi, int userNo, String startDate, String endDate){
+		
+		ArrayList<EggPayMent> list = new ArrayList<EggPayMent>();
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectSearchList");
+		
+		try {
+            pstmt = conn.prepareStatement(sql);
+            
+            int startRow = (pi.getCurrentPage() - 1)*pi.getBoardLimit() + 1;
+            int endRow = startRow + pi.getBoardLimit() - 1;
+            
+            pstmt.setInt(1, userNo);
+            pstmt.setString(2, startDate);
+            pstmt.setString(3, endDate);
+            pstmt.setInt(4, startRow);
+            pstmt.setInt(5, endRow);
+            
+            rset = pstmt.executeQuery();
+            
+            while(rset.next()) {
+              list.add(new EggPayMent(rset.getInt("pay_no"),
+            		  				  rset.getInt("mem_no"),
+            		  				  rset.getInt("price"),
+            		  				  rset.getInt("point"),
+            		  				  rset.getString("payoption"),
+            		  				  rset.getString("paydate"),
+            		  				  rset.getString("category")));
+            }
+            
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }finally {
+            close(rset);
+            close(pstmt);
+         }
+         return list;
+		
+	}
+
 
 }
 
